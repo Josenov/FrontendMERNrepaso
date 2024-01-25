a3cd733b043f4b4dd955fbece0a2a9aafd97d756
Josenov <josenovillo88@gmail.com>
2024-01-25 09:05:42 -0300

    configuracion inicial react

---
+++ FrontendRepaso/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center" >
+        <p className='text-3xl text-red-600 font-extrabold'>Texto de Prueba</p>
+      </div>
+    </>
+  )
+}
+
+export default App
